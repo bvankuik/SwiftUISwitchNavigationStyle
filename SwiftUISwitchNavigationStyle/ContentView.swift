1b1e9db9f6d87ad57b25bf7c653f348411df5bcb
Bart van Kuik <bart@vankuik.nl>
2019-08-30 12:44:12 +0200

    Basic example

--- SwiftUISwitchNavigationStyle/ContentView.swift
+++ SwiftUISwitchNavigationStyle/ContentView.swift
@@ -8,14 +8,55 @@
 
 import SwiftUI
 
+struct Person: Identifiable {
+    let id: Int
+    let name: String
+}
+
+let persons: [Person] = [
+    Person(id: 0, name: "Gretchen"),
+    Person(id: 1, name: "Rowena"),
+    Person(id: 2, name: "Jeni"),
+    Person(id: 3, name: "Kaye"),
+    Person(id: 4, name: "Kenneth")
+]
+
+struct PersonDetail: View {
+    var person: Person
+    var body: some View {
+        Text("More info about \(person.name)")
+    }
+}
+
+struct PersonRow: View, Identifiable {
+    var id: Int {
+        person.id
+    }
+    var person: Person
+    var body: some View {
+        Text("\(person.name)")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List(persons) { person in
+                NavigationLink(destination: PersonDetail(person: person)) {
+                    PersonRow(person: person)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone SE")
+            ContentView()
+                .previewDevice("iPad Pro (9.7-inch)")
+        }
     }
 }
