3063d1d22ddcb19f7ba4746ceeeba545c736c0b6
Bart van Kuik <bart@vankuik.nl>
2019-08-30 12:25:57 +0200

    Initial Commit

---
+++ SwiftUISwitchNavigationStyle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISwitchNavigationStyle
+//
+//  Created by bartvk on 30/08/2019.
+//  Copyright © 2019 DutchVirtual. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
